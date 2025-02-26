9d2f0d32c110dcf398140d036c8e37bcf8fc7251
samke-glitch <sammythemwa@gmail.com>
2025-02-26 12:03:36 +0300

    chg: add-to-cart bttn

--- features/searchLaptop.feature
+++ features/searchLaptop.feature
@@ -11,3 +11,7 @@ Feature: Search Laptop
     And I type "dell laptop 20000" into the search field
     And I click the search button
     Then I should see search results for "dell laptop 20000"
+    When I click on the first search result item
+    And I add the item to the cart
+    Then I should see the success message "Successfully Added to Shopping Cart"
+    And I should see the cart notification icon
--- step_definitions/searchLaptop.steps.ts
+++ step_definitions/searchLaptop.steps.ts
@@ -25,3 +25,33 @@ Then("I should see search results for {string}", (query: string) => {
   I.waitForElement(".result-listings-wrapper", 5);
   I.see(`results for ${query}`);
 });
+
+// Step: Click on the first search result item
+When("I click on the first search result item", () => {
+  // Wait for listings to load and click the first item
+  I.waitForElement('.listing-item', 5);
+  I.click(locate('.listing-item').first().find('a'));
+  I.wait(5); // Allow time for the new tab to open
+  I.switchToNextTab(); // Switch control to the new tab where the product is displayed
+});
+
+// Step: Add the item to the cart
+When("I add the item to the cart", () => {
+  // Wait for the product page to load and click the "Add to Cart" button
+  I.waitForElement('button.opt-btn.yellow-btn', 5);
+  I.click('button.opt-btn.yellow-btn');
+});
+
+// Step: Verify the item is in the cart
+Then("I should see the success message {string}", (message: string) => {
+  // Wait for the transient success message to appear
+  I.waitForText(message, 5);
+});
+
+// Step: Verify the cart notification icon is visible
+Then("I should see the cart notification icon", () => {
+  // Additionally, check that the cart notification icon is present
+  I.waitForElement('img.opt-btn-icon', 5);
+  I.seeElement('img.opt-btn-icon');
+});
+
